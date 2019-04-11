6111eb0b4b2c88035f5350a8fef7c4de49a4b808
DV <DVislearning@gmail.com>
2019-04-11 02:27:08 -0700

    set css design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="App-container">
+          <div id="header">
+            <div id="header-text">DV'S MARKDOWN PREVIEWER<div id="header-blink">_</div></div>
+          </div>
+          <div id="content">
+            <div id="editor-section">
+              <div id="editor-header">E D I T O R</div>
+              <textarea id="editor" autoFocus></textarea>
+            </div>
+            <div id="preview">
+              <div id="preview-header">P R E V I E W</div>
+              <div id="preview-output"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
